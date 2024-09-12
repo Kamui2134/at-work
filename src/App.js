c5e7c1821446ff37ed7762d2a279ed7434835f94
Kamui2134 <ctorkey@gmail.com>
2024-09-12 21:44:57 +0300

    Header is done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header/Header'
+import { Outlet } from 'react-router-dom';
+
 
 function App() {
   return (
 		<div className='App'>
 			<Header />
-			<div id="">
+			<div id="detail">
 				<Outlet />
 			</div>
 		</div>
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,35 @@
 import React from 'react'
+import logo from 'assets/icons/logo.svg'
+import heart from 'assets/icons/heart.svg'
+import bell from 'assets/icons/bell.svg'
+import temporary from 'assets/images/temporary.jpg'
 import './Header.css'
 
 export default function Header() {
-  return (
-    <header className='header'>
-        <div className='container'>
-            
-        </div>
-    </header>
-  )
+	return (
+		<header className='header'>
+			<div className='container'>
+				<div className='logo-container'>
+					<img src={logo} />
+					<h1 className='h1 logo'>
+						at-<b>work</b>
+					</h1>
+				</div>
+				<div className='profile-container'>
+					<div className='links-container'>
+						<a href='#'>
+							<img className='icon' src={bell} alt='favorites' />
+						</a>
+						<a href='#'>
+							<img className='icon' src={heart} alt='favorites' />
+						</a>
+					</div>
+					<div className='avatar-container'>
+						<img className='avatar' src={temporary} alt='avatar' />
+						<p className='text-2 semibold'>Ivan1234</p>
+					</div>
+				</div>
+			</div>
+		</header>
+	)
 }
